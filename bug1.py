9eb09ad6e716e6a894995c7f0a0d848db99a2907
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-10-14 14:09:39 -0700

    Create bug1.py

---
+++ bug1.py
@@ -0,0 +1,41 @@
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+    def shape(self):
+        return "This is a circle"
+
+
+class Circle(Base):
+    def __init__(self, x, y, size):
+        # Call the Base class constructor
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})
+{self.size}
+, - ~ ~ ~ - ,
+, '          ' ,
+,            ,
+,            ,
+,            ,
+,            ,
+,            ,
+,            ,
+,            ,
+,            , '
+' - , _ _ _ , '
+"""
+
+
+def main():
+    c = Circle(1, 2, 3)
+    print(c.shape())
+    print(c.draw())
+
+
+if __name__ == "__main__":
+    main()
