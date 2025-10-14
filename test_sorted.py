910f5e1db1796290b54a4e5c16ac4362becac333
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-10-14 13:53:59 -0700

    Create test_sorted.py

---
+++ test_sorted.py
@@ -0,0 +1,5 @@
+from sorted import reverse_sort_dictionary 
+
+input1 + ['Tom': (5464512, 24), 'Sara': (5446987, 32),  'Mary': (1557896, 20)]
+result = reverse_sort_dictionary(input1)
+print(result)
