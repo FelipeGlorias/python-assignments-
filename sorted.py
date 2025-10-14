681abf8b7e376c9aa8be7240206120fbac6d93bf
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-10-14 13:58:54 -0700

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,17 +1,27 @@
 def reverse_sort_dictionary(data):
-  if not isinstance(data, dict):
-    raise TypeError("Input needs to be a  dictionary")
-
-result = []
-for name, value in data.items():
-  if not isinstance(name, str):
-    raise TypeErro("Dictionary string needs to be names")
-  if not isinstance(value, tuple) or len(value)1+2:
-    raise TypeError("Dictionary needs to be tuples and 2 elements")
-
-phone, age = value 
-result.append((name, phone))
-
-result.sort(key=lambda x: x[0], reverse=True)
-
-return result 
+    
+    if not isinstance(data, dict):
+        raise TypeError("Input must be a dictionary")
+    
+    result = []
+    
+    
+    for name, value in data.items():
+       
+        if not isinstance(name, str):
+            raise TypeError("Dictionary keys must be strings (names)")
+        
+        
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("Dictionary values must be tuples with 2 elements")
+        
+        
+        phone, age = value
+        
+        
+        result.append((name, phone))
+    
+    
+    result.sort(key=lambda x: x[0], reverse=True)
+    
+    return result
