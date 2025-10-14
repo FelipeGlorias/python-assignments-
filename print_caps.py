d11a2b13565c02f26aaf043623a14601f7ed7bab
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-10-14 14:49:51 -0700

    Update print_caps.py

--- print_caps.py
+++ print_caps.py
@@ -1,7 +1,8 @@
 def allcaps(func):
-  def wrapper():
-    result = func()
-    if isinstance(result, str):
-      return result.upper()
-      return result
-      return wrapper
+   
+    def wrapper():
+        result = func()
+        if isinstance(result, str):
+            return result.upper()
+        return result
+    return wrapper
