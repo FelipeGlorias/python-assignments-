1c848d32ba7966890958e4542ca17baf24ac8649
FelipeGlorias <felipe.glorias2000@gmail.com>
2025-10-14 18:11:20 -0700

    Update test_cacti.py

--- test_cacti.py
+++ test_cacti.py
@@ -1,24 +1,24 @@
-from cacti import cacti_number 
+from cacti import cacti_number
 
-@cacti_number 
-def plat1():
-  return [
-    [1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0]
-  ]
+@cacti_number
+def plot1():
+    return [
+        [1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0]
+    ]
 
 @cacti_number
 def plot2():
-  return [
-    [0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [1, 0, 1, 0, 0, 1]
-  ]
+    return [
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [1, 0, 1, 0, 0, 1]
+    ]
 
 def main():
-  print(plot1())
-  print(plot2())
+    print(plot1())  # expected: 4
+    print(plot2())  # expected: 1
 
 if __name__ == "__main__":
-  main()
+    main()
